c9012d806317f210c685cc8ce38438d534ed3d34
Arjan Chakrabory <aryaarjan1@gmail.com>
2025-07-09 22:48:55 +0530

    New Commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,31 @@ class  Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-           appBar: AppBar(centerTitle:true,backgroundColor:Colors.orange,
-             title: Text("MediTech",textAlign:TextAlign.center, )
-           ),
+           appBar:AppBar(),
+        body:Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration:BoxDecoration(
+          image: DecorationImage(
+              image:
+              AssetImage('images/image.png'),
+              fit: BoxFit.cover
+
+          ),
+    ),
+    child: Align(alignment: Alignment(0.1, -0.2),child:Column(
+
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text("MEDITECH", style: TextStyle(fontSize: 45, fontFamily: 'Azonix')),
+        SizedBox(height: 0),
+        Text("Your Smart Health Companion", style: TextStyle(fontFamily: 'Azonix',fontSize: 15)),
+      ],
+    ),
+    ),),
+
     );
   }
 }
