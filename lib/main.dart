90f85987a22f80469174b640024ae7f58d62d33e
Arjan Chakrabory <aryaarjan1@gmail.com>
2025-07-11 12:34:49 +0530

    7th PUSh

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,16 @@ class  Home extends StatelessWidget {
         Text("MEDITECH", style: TextStyle(fontSize: 45, fontFamily: 'Azonix')),
         SizedBox(height: 0),
         Text("Your Smart Health Companion", style: TextStyle(fontFamily: 'Azonix',fontSize: 15)),
+        SizedBox(height: 1,),
+        ElevatedButton(onPressed: (){
+          Navigator.push(context, 
+            MaterialPageRoute(builder: (context)=>LoginPage(),)          
+          );
+        },
+            child: Text("GET STARTED",),
+            style: ElevatedButton.styleFrom(
+
+            ),)
       ],
     ),
     ),),
@@ -36,5 +46,50 @@ class  Home extends StatelessWidget {
     );
   }
 }
+class LoginPage extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+      height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+
+        ),
+        child: Align(
+          alignment: Alignment(0.1, -0.2),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Center(
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Enter your username here",
+
+                  ),
+
+                ),
+              ),
+              Center(
+                child: TextField(
+
+                  decoration: InputDecoration(
+                    hintText: "Enter your password"
+                  ),
+                ),
+              )
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 
 
